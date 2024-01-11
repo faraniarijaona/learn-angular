cd981e9f199c99f6e5ceb43dd684abdf0601aa1b
Fety FARANIARIJAONA <fety.faraniarijaona@kapela.fr>
2024-01-11 17:00:13 +0100

    formatting text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,24 @@ import { FaceSnap } from './models/face-snap.models';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  mySnap!: FaceSnap;
-  mySnap2!: FaceSnap;
+  mySnaps!: FaceSnap[];
 
   ngOnInit(){
-    this.mySnap = {
+    this.mySnaps = [{
       title: 'Archibald',
       description: 'Mon meilleur ami depuis tout petit !',
       createdDate: new Date(),
-      snaps: 0,
+      snaps: 255,
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       location : 'Paris'
-    };
+    },
 
-    this.mySnap2 = {
+   {
       title: 'Archibald',
       description: 'Mon meilleur ami depuis tout petit !',
       createdDate: new Date(),
-      snaps: 4,
+      snaps: 160,
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg'
-    };
+    }];
   }
 }
